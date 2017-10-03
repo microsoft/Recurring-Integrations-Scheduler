30eb3ffe3f740637be57fb19b4006afca5c91721
Tomek Melissa <tmelissa@microsoft.com>
2017-10-03 14:49:33 +0200

    handling of settings upgrade

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -15,6 +15,12 @@ namespace RecurringIntegrationsScheduler
         [STAThread]
         private static void Main()
         {
+            if (Properties.Settings.Default.UpdateSettings)
+            {
+                Properties.Settings.Default.Upgrade();
+                Properties.Settings.Default.UpdateSettings = false;
+                Properties.Settings.Default.Save();
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
--- Scheduler/Properties/Settings.Designer.cs
+++ Scheduler/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RecurringIntegrationsScheduler.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -216,6 +216,18 @@ namespace RecurringIntegrationsScheduler.Properties {
             }
             set {
                 this["AadApplications"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UpdateSettings {
+            get {
+                return ((bool)(this["UpdateSettings"]));
+            }
+            set {
+                this["UpdateSettings"] = value;
             }
         }
     }
